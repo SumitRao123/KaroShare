ae6de3415f3f4b10bc1c2786eb92f59dc9fdafc7
amit kherkar <sumitkherkar169@gmail.com>
2022-07-26 20:13:40 +0530

    add cors

--- App.js
+++ App.js
@@ -2,10 +2,20 @@ const express = require("express");
 const app = express();
 const path =  require("path");
 const PORT = process.env.PORT || 3000;
+const cors =  require("cors");
 
 app.use(express.json());
 const connectDb = require("./Config/db.");
 connectDb();
+
+
+const corsOptions = {
+    origin : process.env.ALLOWED_CLIENTS.split(',')
+
+}
+app.use(cors(cors(corsOptions)));
+
+
 app.use(express.static('public'));
 console.log();
 app.set()
